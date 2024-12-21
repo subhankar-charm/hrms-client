830ea222f1882805baf61fd739d9436067dccd33
subhankar-fleapo <subhankar.charm@gmail.com>
2024-12-22 01:24:48 +0530

    fix: eslint rules and fixes

--- apps/auth/app/page.tsx
+++ apps/auth/app/page.tsx
@@ -1,10 +1,12 @@
 import Avatar from '@repo/ui/avatar';
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Avatar />
       <p className="text-3xl font-bold underline">AUTH APP</p>;
     </>
   );
-}
+};
+
+export default Home;
